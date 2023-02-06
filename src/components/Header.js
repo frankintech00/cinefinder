86c7646857c578faec8b797a72db3cd2686d7a5a
Frank Kelly <frankintech00@gmail.com>
2023-02-06 15:18:45 +0000

    added darkmode button and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import icons8movie48 from "../assets/icons8-movie-48.png";
 
 export const Header = () => {
   const [hidden, setHidden] = useState(true);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
   const activeClass =
     "block py-2 pl-3 pr-4 text-white text-xl bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
   const inactiveClass =
     "block py-2 pl-3 pr-4 text-gray-700 text-xl rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
+      <nav className="bg-white border-b-2 border-gray-200 shadow px-2 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-2 dark:shadow-lg">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           {/* Logo and Name */}
-          <NavLink to="/" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img
               src={icons8movie48}
               className="h-10 mr-3 sm:h-9"
@@ -22,9 +34,44 @@ export const Header = () => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               CineFinder
             </span>
-          </NavLink>
+          </Link>
           {/* Logo and Name */}
           <div className="flex md:order-2">
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
+              type="button"
+              data-toggle-dark="light"
+              className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              {darkMode ? (
+                <svg
+                  aria-hidden="true"
+                  data-toggle-icon="sun"
+                  className="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  aria-hidden="true"
+                  data-toggle-icon="moon"
+                  className="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              )}
+            </button>
             <button // Search Button hidden above md
               onClick={() => setHidden(!hidden)}
               type="button"
