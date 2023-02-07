bffa66f3d86feb4963696eccf764f1c492a6ee5d
Frank Kelly <frankintech00@gmail.com>
2023-02-07 00:04:01 +0000

    fixed search errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -126,6 +126,7 @@ export const Header = () => {
                 <input
                   type="text"
                   id="search-navbar"
+                  name="search"
                   className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search for a movie..."
                   autoComplete="off"
@@ -182,6 +183,7 @@ export const Header = () => {
                 <input
                   type="text"
                   id="search-navbar"
+                  name="search"
                   className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search for a movie..."
                   autoComplete="off"
