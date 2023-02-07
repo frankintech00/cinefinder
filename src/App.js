05b18f2433d99540b31c8a8771b81fa602b1581a
Frank Kelly <frankintech00@gmail.com>
2023-02-07 00:19:59 +0000

    added TODO

--- src/App.js
+++ src/App.js
@@ -13,3 +13,14 @@ function App() {
 }
 
 export default App;
+
+// TODO: 1. Add social links to footer
+// TODO: 2. Tidy up assets folder
+// TODO: 3. Add comments to code
+// TODO: 4. Add a 404 page
+// TODO: 5. Change the favicon
+// TODO: 6. Change page title and meta tags and descriptions
+// TODO: 7. Add a README.md file
+// TODO: 8. Add custom colors to the theme
+// TODO: 9. Host on Netlify
+// TODO: 9. Share on Social Media
